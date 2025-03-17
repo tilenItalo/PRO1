e3bc0512c2afd625a16e87a14a4f0b5b3fbcf4ba
tilenItalo <133925935+tilenItalo@users.noreply.github.com>
2025-03-17 16:20:57 +0100

    Add files via upload

---
+++ Binarne stevilek 1/Binarne stevilek 1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Uporabnik vnese številke
+        Console.WriteLine("Vnesite seznam števil, ločenih s presledkom:");
+        int[] številke = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        // Uporabnik vnese ciljno številko
+        Console.Write("Vnesite ciljno številko: ");
+        int cilj = int.Parse(Console.ReadLine());
+
+        // Poiščemo prvi in zadnji pojav cilja
+        int prviIndeks = PoiščiPrvi(številke, cilj);
+        int zadnjiIndeks = PoiščiZadnji(številke, cilj);
+
+        // Izpišemo rezultat
+        Console.WriteLine($"Ciljna številka {cilj} se pojavi v razponu: [{prviIndeks}, {zadnjiIndeks}]");
+    }
+
+    // Metoda za iskanje prvega pojavljanja cilja v seznamu
+    public static int PoiščiPrvi(int[] seznam, int cilj)
+    {
+        int levi = 0, desni = seznam.Length - 1;
+        int najdeniIndeks = -1;
+
+        while (levi <= desni)
+        {
+            int sredina = (levi + desni) / 2;
+
+            if (seznam[sredina] == cilj)
+            {
+                najdeniIndeks = sredina;
+                desni = sredina - 1;
+            }
+            else if (seznam[sredina] < cilj)
+            {
+                levi = sredina + 1;
+            }
+            else
+            {
+                desni = sredina - 1;
+            }
+        }
+
+        return najdeniIndeks;
+    }
+
+    // Metoda za iskanje zadnjega pojavljanja cilja v seznamu
+    public static int PoiščiZadnji(int[] seznam, int cilj)
+    {
+        int levi = 0, desni = seznam.Length - 1;
+        int najdeniIndeks = -1;
+
+        while (levi <= desni)
+        {
+            int sredina = (levi + desni) / 2;
+
+            if (seznam[sredina] == cilj)
+            {
+                najdeniIndeks = sredina;
+                levi = sredina + 1;
+            }
+            else if (seznam[sredina] < cilj)
+            {
+                levi = sredina + 1;
+            }
+            else
+            {
+                desni = sredina - 1;
+            }
+        }
+
+        return najdeniIndeks;
+    }
+}
---
+++ Binarne stevilek 1/Binarne stevilke 2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Resitev
+{
+    // Metoda za iskanje minimalne kapacitete, ki omogoča prenos vseh paketov v danem številu dni
+    public int MinKapacitetaVDneh(int[] teze, int dnevi)
+    {
+        int minKapaciteta = 0, maxKapaciteta = 0;
+
+        // Določimo začetne meje: najmanjša kapaciteta je največja posamezna teža, največja pa vsota vseh tež
+        for (int i = 0; i < teze.Length; i++)
+        {
+            minKapaciteta = Math.Max(minKapaciteta, teze[i]);
+            maxKapaciteta += teze[i];
+        }
+
+        // Binarno iskanje optimalne kapacitete
+        while (minKapaciteta < maxKapaciteta)
+        {
+            int sredina = (minKapaciteta + maxKapaciteta) / 2;
+            if (LahkoPrenese(teze, dnevi, sredina)) maxKapaciteta = sredina;
+            else minKapaciteta = sredina + 1;
+        }
+        return minKapaciteta;
+    }
+
+    // Preveri, ali je mogoče vse pakete prenesti v danem številu dni z dano kapaciteto
+    private bool LahkoPrenese(int[] teze, int dnevi, int kapaciteta)
+    {
+        int stDni = 1, trenutnaTeza = 0;
+
+        for (int i = 0; i < teze.Length; i++)
+        {
+            // Če bi dodajanje trenutne teže preseglo kapaciteto, začnemo nov dan
+            if (trenutnaTeza + teze[i] > kapaciteta)
+            {
+                stDni++;
+                trenutnaTeza = 0;
+            }
+            trenutnaTeza += teze[i];
+        }
+        return stDni <= dnevi;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Vnesite število paketov:");
+        int n = int.Parse(Console.ReadLine());
+        int[] teze = new int[n];
+
+        Console.WriteLine("Vnesite teže paketov (ločene s presledkom):");
+        string[] vnosi = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++)
+        {
+            teze[i] = int.Parse(vnosi[i]);
+        }
+
+        Console.WriteLine("Vnesite število dni:");
+        int dnevi = int.Parse(Console.ReadLine());
+
+        // Izpis rezultata minimalne kapacitete
+        Console.WriteLine("Minimalna kapaciteta: " + new Resitev().MinKapacitetaVDneh(teze, dnevi));
+    }
+}
